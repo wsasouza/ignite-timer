d15a061c785292d1b21707ed6618ba84ccbbc97f
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-07-11 15:15:01 -0300

    Feat: Configure React Router Dom

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
+  )
+}
---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+
+import { History } from './pages/History'
+import { Home } from './pages/Home'
+
+export function Router() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </div>
+  )
+}
