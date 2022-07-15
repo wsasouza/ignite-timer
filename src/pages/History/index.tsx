0992a28cf585681395bb2e9b0e5ca1f27f35d032
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-07-14 23:02:15 -0300

    Feat: date formatting

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,7 @@
 import { useContext } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { CyclesContext } from '../../contexts/CyclesContext'
 
 import { HistoryContainer, HistoryList, Status } from './styles'
@@ -26,7 +29,12 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount}</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluído</Status>
